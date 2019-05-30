08ddad9307b1767508f3b88495d759577668f572
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-05-30 13:44:53 -0400

    Adds merge of samples script.

--- src/main/python/FullAnalysis.py
+++ src/main/python/FullAnalysis.py
@@ -1,12 +1,11 @@
 import logging
-import os
-import subprocess
 
 import AlignSample
 import BamToBed
 import DownloadSample
 import FilterBam
 import GenomeCoverage
+import MergeSampleBed
 import SplitGenomeCoverage
 import click
 
@@ -16,6 +15,8 @@ import click
               help='Sample names listed one sample name by line.'
               ' The first line is ignored.'
               ' An SRR id can be provided (tab-separated) to download the FASTQ file automatically, otherwise it must be provided.')
+@click.option('--merge', '-m', type=click.Path, default=None,
+              help='Merge name if first columns and sample names to merge on following columns - tab delimited.')
 @click.option('--fasta', '-f', type=click.Path(exists=True), default='sacCer3.fa',
               help='FASTA file used for alignment.')
 @click.option('--sizes', type=click.Path(exists=True), default='sacCer3.chrom.sizes',
@@ -28,7 +29,7 @@ import click
               help='Split reads minimum length.')
 @click.option('--splitMaxLength', default=400,
               help='Split reads maximum length.')
-def main(samples, fasta, sizes, threads, splitlength, splitminlength, splitmaxlength):
+def main(samples, merge, fasta, sizes, threads, splitlength, splitminlength, splitmaxlength):
     '''Analyse Martin et al. data from November 2018 in Genetics.'''
     logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     AlignSample.bwa_index(fasta)
@@ -41,6 +42,15 @@ def main(samples, fasta, sizes, threads, splitlength, splitminlength, splitmaxle
         sample = sample_info[0]
         srr = sample_info[1] if len(sample_info) > 1 else None
         analyse(sample, srr, fasta, sizes, threads, splitlength, splitminlength, splitmaxlength)
+    if merge is not None:
+        with open(merge) as infile:
+            for merge_line in infile:
+                if merge_line.startswith('#'):
+                    continue
+                merge_info = merge_line.split('\t');
+                sample = merge_info[0]
+                MergeSampleBed.merge_samples(sample, merge_info[1:])
+                coverage(sample, sizes, splitlength, splitminlength, splitmaxlength)
 
 
 def analyse(sample, srr, fasta, sizes, threads, splitlength, splitminlength, splitmaxlength):
@@ -50,6 +60,12 @@ def analyse(sample, srr, fasta, sizes, threads, splitlength, splitminlength, spl
     AlignSample.align(sample, fasta, threads)
     FilterBam.filter_bam(sample, fasta, threads)
     BamToBed.bam_to_bed(sample, threads)
+    coverage(sample, sizes, splitlength, splitminlength, splitmaxlength)
+
+
+def coverage(sample, sizes, splitlength, splitminlength, splitmaxlength):
+    '''Compute coverage of a single sample.'''
+    print ('Analyse sample {}'.format(sample))
     GenomeCoverage.genome_coverage(sample, sizes)
     if splitlength is not None:
         SplitGenomeCoverage.split_genome_coverage(sample, sizes, splitlength, splitminlength, splitmaxlength)
---
+++ src/main/python/MergeSampleBed.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import subprocess
+
+import click
+
+
+@click.command()
+@click.option('--merge', '-s', type=click.File('r'), default='merge.txt',
+              help='Merge name if first columns and sample names to merge on following columns - tab delimited.'
+              ' The first line is ignored.')
+def main(merge):
+    '''Merge BED files related to samples.'''
+    logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+    merge_lines = merge.read().splitlines()
+    for merge_line in merge_lines:
+        if merge_line.startswith('#'):
+            continue
+        merge_info = merge_line.split('\t');
+        merge_samples(merge_info[0], merge_info[1:])
+
+
+def merge_samples(name, samples):
+    '''Merge BED files related to samples.'''
+    print ('Merging samples {} into a single sample {}'.format(samples, name))
+    merged_bed_tmp = name + '-tmp.bed'
+    with open(merged_bed_tmp, "w") as outfile:
+        for sample in samples:
+            sample_bed = sample + '-raw.bed'
+            with open(sample_bed, "r") as infile:
+                for line in infile:
+                    outfile.write(line)
+    merged_bed = name + '-raw.bed'
+    cmd = ['bedtools', 'sort', '-i', merged_bed_tmp]
+    logging.debug('Running {}'.format(cmd))
+    with open(merged_bed, "w") as outfile:
+        subprocess.call(cmd, stdout=outfile)
+    if not os.path.isfile(merged_bed):
+        raise AssertionError('Error when sorting BED ' + merged_bed_tmp)
+    os.remove(merged_bed_tmp)
+
+
+if __name__ == '__main__':
+    main()
