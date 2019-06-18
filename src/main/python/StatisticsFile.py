66ade9a8549a2726a8cd2f95e79ff22ab16efb43
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-06-18 16:01:43 -0400

    Adds filtered BED file stats to statistics file.

--- src/main/python/StatisticsFile.py
+++ src/main/python/StatisticsFile.py
@@ -3,6 +3,7 @@ import os
 import re
 import subprocess
 
+import FilterBed
 import FullAnalysis
 import GenomeCoverage
 import SplitGenomeCoverage
@@ -14,28 +15,30 @@ import click
               help='Sample names listed one sample name by line.')
 @click.option('--merge', '-m', type=click.Path(), default='merge.txt',
               help='Merge name if first columns and sample names to merge on following columns - tab delimited.')
+@click.option('--annotations', '-a', type=click.Path(), default='annotations.bed',
+              help='File used for FilterBed script, if any.')
 @click.option('--output', '-o', type=click.Path(), default='statistics.txt',
               help='Output file were statistics are written.')
-def main(samples, merge, output):
+def main(samples, merge, annotations, output):
     '''Creates statistics file for samples.'''
     logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     sample_names = FullAnalysis.first_column(samples)
     merge_names = FullAnalysis.first_column(merge) if os.path.isfile(merge) else None
-    all_statistics(sample_names, merge_names, output)
+    all_statistics(sample_names, merge_names, annotations, output)
 
 
-def all_statistics(samples, merges, output):
+def all_statistics(samples, merges, annotations, output):
     stats_headers = None
     samples_stats = []
     for sample in samples:
         if stats_headers is None:
-            stats_headers = headers(sample)
-        sample_stats = sample_statistics(sample)
-        samples_stats.extend([sample_stats])
+            stats_headers = headers(sample, annotations)
+        sample_stats = sample_statistics(sample, annotations)
+        samples_stats.append(sample_stats)
     if merges:
         for merge in merges:
-            sample_stats = sample_statistics(merge)
-            samples_stats.extend([sample_stats])
+            sample_stats = sample_statistics(merge, annotations)
+            samples_stats.append(sample_stats)
     with open(output, 'w') as out:
         out.write('\t'.join(stats_headers))
         out.write('\n')
@@ -44,16 +47,19 @@ def all_statistics(samples, merges, output):
             out.write('\n')
 
 
-def headers(sample):
+def headers(sample, annotations):
     '''Statistics headers'''
     headers = ['Sample', 'Total reads', 'Mapped reads', 'Deduplicated reads']
     splits = SplitGenomeCoverage.splits(sample)
     splits_headers = [split[len(sample) + 1:] for split in splits]
     headers.extend(splits_headers)
+    filtereds = FilterBed.filtered(sample, annotations)
+    filtereds_headers = [filtered[len(sample) + 1:] for filtered in filtereds]
+    headers.extend(filtereds_headers)
     return headers
     
     
-def sample_statistics(sample):
+def sample_statistics(sample, annotations):
     '''Statistics of a single sample.'''
     print ('Computing statistics for sample {}'.format(sample))
     sample_stats = [sample]
@@ -66,6 +72,11 @@ def sample_statistics(sample):
     splits = SplitGenomeCoverage.splits(sample)
     if splits:
         beds = [split + '-raw.bed' for split in splits]
+        counts = [GenomeCoverage.count_bed(bed) for bed in beds]
+        sample_stats.extend(counts)
+    filtereds = FilterBed.filtered(sample, annotations)
+    if filtereds:
+        beds = [filtered + '.bed' for filtered in filtereds]
         counts = [GenomeCoverage.count_bed(bed) for bed in beds]
         sample_stats.extend(counts)
     return sample_stats
