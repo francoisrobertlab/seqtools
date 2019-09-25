13815386e99657accfac8538d8808cf583eca98f
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-09-25 15:35:58 -0400

    Adds setup configuration for pip.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SeqTools",
+    version="0.1",
+    packages=find_packages(),
+    author="Christian Poitras",
+    author_email="christian.poitras@ircm.qc.ca",
+    description="Tools to analyze NGS data",
+    keywords="bioinformatics, NGS",
+    url="https://github.com/francoisrobertlab/seqtools",
+    license='GNU General Public License version 3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License version 3'
+    ]
+)
