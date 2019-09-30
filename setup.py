79e52111ab576a7b3a9e213d68a56393ab9a1ef6
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-09-30 15:22:08 -0400

    Change version to 0.1.1-SNAPSHOT.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SeqTools",
-    version="0.2-SNAPSHOT",
+    version="0.1.1-SNAPSHOT",
     packages=find_packages(),
     author="Christian Poitras",
     author_email="christian.poitras@ircm.qc.ca",
