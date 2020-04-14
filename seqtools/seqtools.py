010030a16cd0c687db0c1f29f03c2ad8e68b5300
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-04-14 07:08:30 -0400

    Fixes intersect annotations script.

--- seqtools/seqtools.py
+++ seqtools/seqtools.py
@@ -1,6 +1,6 @@
 import click
 
-from seqtools import Bam2Bed, Bowtie2, Bwa, Download, FilterBam, GenomeCoverage, Intersect, Merge, MergeBigwigs, Plot2do, SlowSplit, Split, Statistics, Vap
+from seqtools import Bam2Bed, Bowtie2, Bwa, Download, FilterBam, GenomeCoverage, Intersect, IntersectAnnotations, Merge, MergeBigwigs, Plot2do, SlowSplit, Split, Statistics, Vap
 
 
 @click.group()
@@ -15,7 +15,7 @@ seqtools.add_command(Download.download)
 seqtools.add_command(FilterBam.filterbam)
 seqtools.add_command(GenomeCoverage.genomecov)
 seqtools.add_command(Intersect.intersect)
-seqtools.add_command(IntersectAnnotation.intersectannotations)
+seqtools.add_command(IntersectAnnotations.intersectannotations)
 seqtools.add_command(Merge.merge)
 seqtools.add_command(MergeBigwigs.mergebw)
 seqtools.add_command(Plot2do.plot2do)
