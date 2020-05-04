d44b2dd04767672c586ab05298644ca60a33021b
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-05-04 11:28:10 -0400

    Fixes sbatch output for dyad coverage bash script.

--- bash/dyadcoverage.sh
+++ bash/dyadcoverage.sh
@@ -5,8 +5,8 @@
 #SBATCH --mem=16G
 #SBATCH --mail-user=christian.poitras@ircm.qc.ca
 #SBATCH --mail-type=ALL
-#SBATCH --output=firstdyadcoverage-%A_%a.out
-#SBATCH --error=firstdyadcoverage-%A_%a.out
+#SBATCH --output=dyadcoverage-%A_%a.out
+#SBATCH --error=dyadcoverage-%A_%a.out
 
 args+=("$@")
 if [ ! -z "$SLURM_ARRAY_TASK_ID" ]
