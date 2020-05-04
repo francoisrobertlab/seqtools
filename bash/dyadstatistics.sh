3602bf2097c365065c4a15a2d4967a53d6a9c2be
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-05-04 11:27:45 -0400

    Adds dyad statistics bash script.

---
+++ bash/dyadstatistics.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=def-robertf
+#SBATCH --array=0-0
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
+#SBATCH --mail-user=christian.poitras@ircm.qc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --output=dyadcoverage-%A_%a.out
+#SBATCH --error=dyadcoverage-%A_%a.out
+
+args+=("$@")
+if [ ! -z "$SLURM_ARRAY_TASK_ID" ]
+then
+  args+=("-i" "$SLURM_ARRAY_TASK_ID")
+fi
+
+mnasetools dyadstatistics "${args[@]}"
