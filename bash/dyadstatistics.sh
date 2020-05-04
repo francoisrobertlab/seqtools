bc68d60ce34d2dffa3ea0ae9837159bc694b29a4
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-05-04 12:31:08 -0400

    Removes array usage from dyadstatistics.sh.

--- bash/dyadstatistics.sh
+++ bash/dyadstatistics.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH --account=def-robertf
-#SBATCH --array=0-0
-#SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
 #SBATCH --mail-user=christian.poitras@ircm.qc.ca
 #SBATCH --mail-type=ALL
-#SBATCH --output=dyadcoverage-%A_%a.out
-#SBATCH --error=dyadcoverage-%A_%a.out
+#SBATCH --output=dyadcoverage-%A.out
+#SBATCH --error=dyadcoverage-%A.out
 
 args+=("$@")
 if [ ! -z "$SLURM_ARRAY_TASK_ID" ]
