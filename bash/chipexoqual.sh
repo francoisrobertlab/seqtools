dd7abc3098312c6416a8b8cf9cd4e08094292194
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-09-24 12:42:52 -0400

    Fixes index parameter in chipexoqual.sh.

--- bash/chipexoqual.sh
+++ bash/chipexoqual.sh
@@ -11,7 +11,7 @@
 args=("$@")
 if [ ! -z "$SLURM_ARRAY_TASK_ID" ]
 then
-  args+=("-i" "$SLURM_ARRAY_TASK_ID")
+  args+=("--index" "$SLURM_ARRAY_TASK_ID")
 fi
 if [ ! -z "$SLURM_CPUS_PER_TASK" ]
 then
