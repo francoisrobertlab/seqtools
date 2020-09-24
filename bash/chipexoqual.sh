ac27961b518362e943a81eac89c9768dbee853d6
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-09-24 17:03:42 -0400

    Improves sbatch settings for chipexoqual.

--- bash/chipexoqual.sh
+++ bash/chipexoqual.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-robertf
 #SBATCH --time=24:00:00
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
 #SBATCH --mail-user=christian.poitras@ircm.qc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --output=chipexoqual-%A_%a.out
