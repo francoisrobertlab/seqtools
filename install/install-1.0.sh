ebdd3e5a5c376d189a29e5b38c05a19851d8de32
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-10-07 07:13:47 -0400

    Adds installation script for version 1.0.

---
+++ install/install-1.0.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+VENV="$HOME/seqtools-venv"
+BASH="$VENV"/bash
+SEQTOOLS="$VENV"/seqtools
+SEQTOOLS_BASH="$SEQTOOLS"/bash
+EMAIL="$JOB_MAIL"
+
+if [ "$1" == "clean" ]
+then
+    echo "Removing python virtual environment at $VENV"
+    rm -R "$VENV"
+    exit 0
+fi
+if [[ ! "$EMAIL" =~ ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$ ]]
+then
+    echo "Could not find your email address. Did you run configure.sh?"
+    exit 1
+fi
+if [ ! -d "$VENV" ]
+then
+    echo "Creating python virtual environment at $VENV"
+    python3 -m venv "$VENV"
+fi
+echo "Updating python libraries"
+pip uninstall -y SeqTools
+pip install git+https://git@github.com/francoisrobertlab/seqtools.git@1.0
+echo "Updating bash scripts"
+rm -R "$BASH"
+mkdir "$BASH"
+git clone --depth 1 --branch 1.0 https://github.com/francoisrobertlab/seqtools.git "$SEQTOOLS"
+cp "$SEQTOOLS_BASH"/*.sh "$BASH"
+find "$BASH" -type f -name "*.sh" -exec sed -i "s/christian\.poitras@ircm\.qc\.ca/$EMAIL/g" {} \;
+rm -Rf "$SEQTOOLS"
