d11d65bc4f50a5456f5ec8c02d703ba0df292c5e
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-10-16 16:03:54 -0400

    Set memory usage in statistics bash script.

--- bash/statistics.sh
+++ bash/statistics.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-robertf
 #SBATCH --time=24:00:00
+#SBATCH --mem=8G
 #SBATCH --mail-user=christian.poitras@ircm.qc.ca
 #SBATCH --mail-type=ALL
 #SBATCH --output=statistics-%A.out
