bd18e5732d05e44edf5f38db7e038dc538674cfe
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-11-30 17:38:24 -0500

    Allows install.sh to install any version of seqtools.

--- install/install.sh
+++ install/install.sh
@@ -5,6 +5,7 @@ BASH="$VENV"/bash
 SEQTOOLS="$VENV"/seqtools
 SEQTOOLS_BASH="$SEQTOOLS"/bash
 EMAIL="$JOB_MAIL"
+BRANCH="master"
 
 if [ "$1" == "clean" ]
 then
@@ -22,13 +23,22 @@ then
     echo "Creating python virtual environment at $VENV"
     python3 -m venv "$VENV"
 fi
+if [ ! -z "$1" ]
+then
+    BRANCH="$1"
+    echo "Selected branch $BRANCH for seqtools"
+fi
 echo "Updating python libraries"
 pip uninstall -y SeqTools
-pip install git+https://git@github.com/francoisrobertlab/seqtools.git
+pip install git+https://git@github.com/francoisrobertlab/seqtools.git@"$BRANCH"
 echo "Updating bash scripts"
 rm -R "$BASH"
 mkdir "$BASH"
-git clone --depth 1 https://github.com/francoisrobertlab/seqtools.git "$SEQTOOLS"
+git clone --depth 1 --branch "$BRANCH" https://github.com/francoisrobertlab/seqtools.git "$SEQTOOLS"
 cp "$SEQTOOLS_BASH"/*.sh "$BASH"
 find "$BASH" -type f -name "*.sh" -exec sed -i "s/christian\.poitras@ircm\.qc\.ca/$EMAIL/g" {} \;
 rm -Rf "$SEQTOOLS"
+if [ ! -f "$SEQTOOLS_BASH"/install.sh ]
+then
+    rm "$BASH"/install.sh
+fi
