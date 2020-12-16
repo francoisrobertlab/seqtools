d50cf0ca9b2e4aecbc79f8562f2c3aa228fb0e78
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-12-16 09:19:10 -0500

    Updates version to 1.1 in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SeqTools',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     author='Christian Poitras',
     author_email='christian.poitras@ircm.qc.ca',
