78e9bc14d72961cfeacbba6d49bf87ec491b99f7
Christian Poitras <christian.poitras@ircm.qc.ca>
2020-12-16 09:03:39 -0500

    Adds moveannotations bash script.

---
+++ bash/moveannotations.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --account=def-robertf
+#SBATCH --time=24:00:00
+#SBATCH --array=0-0
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4G
+#SBATCH --mail-user=christian.poitras@ircm.qc.ca
+#SBATCH --mail-type=ALL
+#SBATCH --output=moveannotations-%A_%a.out
+#SBATCH --error=moveannotations-%A_%a.out
+
+args=("$@")
+if [ ! -z "$SLURM_ARRAY_TASK_ID" ]
+then
+  args+=("-i" "$SLURM_ARRAY_TASK_ID")
+fi
+
+# Usual parameters
+# -d 6 -rn -dn
+exotools moveannotations "${args[@]}"
